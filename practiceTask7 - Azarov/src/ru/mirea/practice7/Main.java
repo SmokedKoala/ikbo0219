343e5e6e9882c7b8031d467d2b0feba5bec235da
SmokedKoala <k.azarov@inbox.ru>
2020-11-02 12:32:05 +0300

    practices 6 and 7

---
+++ practiceTask7 - Azarov/src/ru/mirea/practice7/Main.java
@@ -0,0 +1,16 @@
+package ru.mirea.practice7;
+
+public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("----------Console (stack realization)----------");
+//        System.out.println("Enter two decks of cards (player1 and player2):");
+//        new StackGame(false).play();
+//    }
+
+    public static void main(String[] args) {
+        new GameInterface();
+    }
+}
+
+//1 3 5 7 9
+//2 4 6 8 0
